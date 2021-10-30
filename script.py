a4aaff1f78c2a7ea9142b12ca82ed07216bc6d73
MohammadAkmal303 <makmalm@webmail.umm.ac.id>
2021-10-30 19:02:00 +0700

    Plus Source Code
    Menambahkan import numpy dan glob

--- script.py
+++ script.py
@@ -1,6 +1,14 @@
+import numpy as np
 import cv2
+import glob
 
 img_dir1 = "Dataset/jeruknipis/"
 img_dir2 = "Dataset/Seledri/"
 
 ext = ['jpg', 'png']
+
+files1 = []
+files2 = []
+
+[files1.extend(glob.glob(img_dir1 + '*.' + e)) for e in ext]
+[files2.extend(glob.glob(img_dir2 + '*.' + e)) for e in ext]
